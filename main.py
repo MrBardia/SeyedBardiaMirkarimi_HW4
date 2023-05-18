afbc498f2d8dcc038ffca7143985d3fcbfda9219
bardia mirkarimi <bardia.mirkarimi@gmail.com>
2023-05-18 15:28:12 +0330

    compelet signup and login methods with account info presenting option

--- main.py
+++ main.py
@@ -1,38 +1,44 @@
 #! usr/bin/python3
 
 from users import Accounts
-import users
 from getpass import getpass
-import hashlib
-from typing import Any
-import uuid
 
 
 def main():
     def get_password():
-        password = None
-        while not password:
-            password = getpass("Enter Password: ")
+        while True:
+            new_password = getpass("Enter Password: ")
             password_confirm = getpass("Confirm Password: ")
-            if password != password_confirm:
+            if new_password != password_confirm:
                 print("Password don't match!")
-                password = None            
-        return password  
-    
+            break
+        return new_password
     while True:
-        i = input("for finishing program enter 0, for signing up enter 1, for account check enter 2: ")
-        match i:
+        option = input("Enter your Request Num (0 to exit, 1 to signup, 2 to login): ")
+        match option:
             case "0":
                 break
             case "1":
-                  username = input("Enter your username: ")
-                  password = get_password()
-                  phone_number = input("Enter your phone Number: ")
-                  
-                  account1 = Accounts(username, password, phone_number)
-                  account1.new_account(username) 
-                  print(users.Account.account_dict)               
+                username = input("Enter username: ")
+                password = get_password()
+                phone_number = input("Enter Phone Number Please (Not necessary): ")
+                Accounts.new_account(username, password, phone_number)
             case "2":
-                    pass
-             
+                log_in_username = input("Please enter your username: ")
+                log_in_password = getpass("Please Enter Your password: ")
+                if Accounts.login(log_in_username, log_in_password):
+                    while True:
+                        log_in_methods = input("Enter 1:info, 2:user and phone edit, 3:password reset, 4:exit : ")
+                        match log_in_methods:
+                            case "1":
+                                logged_user = Accounts.account_dict[log_in_username]
+                                print(logged_user)
+                            case "2":
+                                pass
+                            case "3":
+                                pass
+                            case "4":
+                                break
 
+
+main()
--- users.py
+++ users.py
@@ -1,72 +1,79 @@
 #! usr/bin/python3
 
-from getpass import getpass
+
 import hashlib
-from typing import Any
 from uuid import uuid4
 
 
-
-class Accounts():
-
-    account_dict = {}    
+class Accounts:
+    account_dict = {}
 
     def __init__(self, username: str, __password: str, phone_number=None) -> None:
         self.username = username
-        self.__password = __password        
+        self.__password = __password
         self.phone_number = phone_number
         self.id = uuid4()
 
-    
+    @staticmethod
+    def build_pass(password):
+        pass_hash = hashlib.sha256(str.encode(password)).hexdigest()
+        return pass_hash
+
     @staticmethod
     def is_valid_username(username, dictionary: dict):
         if username in dictionary:
             raise ValueError("username already exists")
-        return True
-    
-    
+        else:
+            return True
+
     @staticmethod
     def is_valid_password(password):
         if len(password) < 4:
             raise ValueError("Password is too short.")
-        return True
-    
-    
-    @classmethod
-    def new_account(cls, username):
-        cls.account_dict[f"{username}"] = cls
+        else:
+            return True
 
-    
+    @classmethod
+    def new_account(cls, username: str, __password: str, phone_number=None):
+        if cls.is_valid_username(username, cls.account_dict):
+            if cls.is_valid_password(__password):
+                password = cls.build_pass(__password)
+                user1 = cls(username, password, phone_number)
+                cls.account_dict[username] = user1
+                return user1
+            else:
+                raise ValueError("Password is too short.")
+        else:
+            raise ValueError("username already exist.")
+
     def update_password(self):
         pass
 
-    
     def update_profile(self, new_user, new_phone):
-        
-        if new_phone == "":
-            new_phone = self.phone_number
-        
-        if new_user == "":
-            new_user = self.username
-        
-        
-        x = Accounts(new_user, self.__password, new_phone, self.id)
-        x.new_account(new_user) 
+        pass
 
+    @classmethod
+    def login(cls, username, password):
+        pass
+        if username in cls.account_dict:
+            user_test = cls.account_dict[username]
+            user_pass = user_test.__repr__()
+            if user_pass == cls.build_pass(password):
+                return True
+            else:
+                raise ValueError("Invalid Password!")
+        else:
+            raise ValueError("Invalid username!")
 
-    def login(self, username, password):
-        password = hashlib.md5(str.encode(password)).hexdigest()
-        if username in self.account_dict:
-            test_object = self.account_dict[f"{username}"]
-        
-        if test_object.__password == password:
-            return True 
-        raise ValueError("No account Found!")
+    def get_password(self):
+        return self.__password
 
-    
     def __str__(self):
         return f"username: {self.username}, phone number: {self.phone_number}, id: {self.id}"
-    
+
+    def __repr__(self):
+        return self.__password
 
 
-
+user = Accounts.new_account("bardia", "1234")
+Accounts.login("bardia", "1234")
